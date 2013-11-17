57af75ab301dc28012aead186e1704104d9b5331
Michał Rudnicki <stronger@epsi.pl>
2013-11-17 13:31:06 +0000

    Bad merge fixed

--- src/Client.php
+++ src/Client.php
@@ -71,50 +71,9 @@ class Client {
 		$this->session->attachSession($params);
 		$this->session->recordRequest($method, $page, $params);
 
-<<<<<<< HEAD
-		// configure request options
-		$ch = curl_init();
-		$url = static::PROTOCOL . static::HOST . $page;
-		$query = http_build_query($params);
-		if ($method === "POST") {
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-		} else {
-			$query and $url .= "?{$query}";
-			curl_setopt($ch, CURLOPT_HTTPGET, true);
-		}
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13");
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-
-		// configure cookies
-		$cookie = $this->session->getCookie();
-		$cookieJar = tempnam(sys_get_temp_dir(), "bia-");
-		file_put_contents($cookieJar, $cookie);
-		curl_setopt($ch, CURLOPT_COOKIEJAR, $cookieJar);
-		curl_setopt($ch, CURLOPT_COOKIEFILE, $cookieJar);
-
-		// make http request and record output
-		$html = curl_exec($ch);
-		$errno = curl_errno($ch);
-		$error = curl_error($ch);
-		curl_close($ch);
-		$cookie = file_get_contents($cookieJar);
-		unlink($cookieJar);
-
-		$this->session->recordResponse($html, $errno);
-		if ($errno) {
-			throw new ClientException("Error requesting HTTP $method $page: $error", $errno);
-		}
-=======
 		// make call and record response
 		$html = $this->connection->call($method, $url, $params, $cookie);
 		$this->session->recordResponse($html, $cookie);
->>>>>>> e5b3602... Separated connection from client, added unit tests
 
 		// build document from returned html and update session with token
 		$document = new Document($html);
